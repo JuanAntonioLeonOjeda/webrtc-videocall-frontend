f6ac46d24d02c57711c26ba3c3da8ea6b248b050
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-09 23:35:43 +0100

    name for AccessForm

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,12 @@
 </template>
 
 <script>
+// import AccessForm from '@/components/AccesForm'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  // components: {
+  //   AccessForm
+  // }
 }
 </script>
